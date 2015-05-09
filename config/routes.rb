a1e8dc0943df2e04b5cf515e9ac561f84f15f5f5
jfaleria <jfaleria@gmail.com>
2015-05-09 16:32:26 +0800

    removed login pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   resources :socialmedia
 
+  get 'pages/info'
+
+  root :to => redirect('/contacts')
   resources :contacts
-  root :to=>redirect('/contacts')
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
